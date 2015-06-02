7ab45c68b2793507db85f4429e9491012434c777
Joe Carter <theupperquartile@gmail.com>
2015-06-02 10:19:05 +0100

    Event firing implemented

--- core/plugins.js
+++ core/plugins.js
@@ -62,6 +62,15 @@ PluginManager.prototype.GetConfig = function() {
     return {}; // TODO: make file based w/ persistence
 }
 
+PluginManager.prototype.FireEvent = function(event, args) {
+    for (var i = 0; i < this.plugins.length; i++) {
+        var plugin = this.plugins[i];
+        if (plugin.loaded) {
+            plugin.FireEvent(event, args);
+        }
+    }
+}
+
 function PluginInfo(manager, directory) {
     this.manager = manager;
     this.directory = directory;
@@ -85,7 +94,7 @@ PluginInfo.prototype.Load = function() {
         this.loaded = true;
 
         this.plugin.events.onLoad(this);
-        console.log("Loaded "+this.meta.name);
+        console.log("Loaded " + this.meta.name);
     }
     catch (e) {
         // TODO: consider removing try/catch, or rethrowing.
@@ -105,9 +114,15 @@ PluginInfo.prototype.GetConfig = function() {
     var manconf = this.manager.GetConfig();
     if (this.meta.name in manconf) { return this.meta.name; }
     // no existing userconfig, clone it before returning
-    
+
     var conf = {}
     if ("config" in this.meta)
         conf = this.meta.config;
     return manconf[this.meta.name] = JSON.parse(JSON.stringify(conf));
 };
+
+PluginInfo.prototype.FireEvent = function(eventname, args) {
+    if (!this.loaded) { return; }
+    if (!(eventname in this.plugin.events)) { return; }
+    this.plugin.events[eventname](args);
+}
