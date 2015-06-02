ed175c8ef24cd3f9278f2359f0193017531434b2
Joe Carter <theupperquartile@gmail.com>
2015-06-02 10:20:25 +0100

    Slack pushing implemented

--- coreplugins/slack/slack.js
+++ coreplugins/slack/slack.js
@@ -1,21 +1,39 @@
 var plugin;
+var slack;
+var config;
 
 var events = {}
 
 events.onLoad = function(plugin) {
     // dependency injection, any better methods for Node?
     this.plugin = plugin;
+    config = plugin.GetConfig();
 
-    console.log("Slack Loaded");
+    // https://www.npmjs.com/package/slack-node
+    var Slack = require('slack-node');
+    slack = new Slack();
+    slack.setWebhook(config.webhookuri);
 }
 
 events.onUnload = function() {
 }
 
-events.plug_onJoin = function(user) {
+events.plug_join = function(user) {
+
 }
 
-events.plug_onMessage = function(user, chatMessage) {
+events.plug_chat = function(message) {
+
+    slack.webhook({
+        channel : config.channel,
+        username : config.username,
+        text : "[" + message.from.username + "] " + message.message
+    }, function(err, response) {
+        if (response.status != "ok") {
+            console.log(response);
+        }
+    });
+
 }
 
 module.exports = {
