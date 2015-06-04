14fb1c49be18caf05b73da500bc656c6567aa035
Joe Carter <theupperquartile@gmail.com>
2015-06-04 01:34:41 +0100

    Implement ping plugin (closes #8)

---
+++ plugins/ping/ping.js
@@ -0,0 +1,30 @@
+var plugin;
+var manager;
+var plug;
+
+var events = {};
+
+events.onLoad = function(_plugin) {
+    plugin = _plugin;
+    manager = plugin.manager;
+    plug = manager.getPlugin("plug").plugin.plug;
+}
+
+// http://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
+var randmsg = [ "Destination network unreachable", "Destination host unreachable", "Destination host unknown" ];
+
+events.plug_command_ping = function(message) {
+    var rand = Math.floor(Math.random() * 10);
+
+    var response = "pong!";
+
+    if (rand == 0) {
+        response = randmsg[Math.floor(Math.random() * randmsg.length)];
+    }
+
+    plug.sendChat("[@" + message.from.username + "] " + response);
+}
+
+module.exports = {
+    "events" : events
+};
