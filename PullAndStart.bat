7b4d1e7c11d4edabc0bdee225df0df9e9576c990
Michael Writhe <michael@writhem.com>
2015-06-17 01:27:19 -0600

    Echoing steps in the PullAndStart.bat

--- PullAndStart.bat
+++ PullAndStart.bat
@@ -1,7 +1,12 @@
 @echo off
 :loop
+echo "Updating from git's http remote..."
 git fetch http
 git reset --hard http/master
+echo "checking npm dependancies..."
+npm install
+echo "Starting BotPlug..."
 node main.js
+echo "Pausing for 60 seconds to restart..."
 PING 1.1.1.1 -n 1 -w 60000 >NUL
 goto loop
