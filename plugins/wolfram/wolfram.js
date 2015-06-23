ca3eac043ad8d1e6a938e9e7f0d7098ee99825c1
Joe Carter <theupperquartile@gmail.com>
2015-06-23 16:48:25 +0100

    Convert wolfram to new format

--- plugins/wolfram/wolfram.js
+++ plugins/wolfram/wolfram.js
@@ -1,37 +1,25 @@
-var plugin;
-var manager;
-var plug;
-var config;
-var http = require('http');
+var PluginInstance = require(__core + "PluginInstance.js");
+var wolfram = new PluginInstance();
 
-var events = {};
+var request = require("request");
+var qs = require("qs");
 
-events.onLoad = function (_plugin) {
-    plugin = _plugin;
-    manager = plugin.manager;
-    plug = manager.getPlugin("plug").plugin.plug;
-    config = plugin.getConfig();
-};
-
-events.plug_command_wa = function (request) {
+wolfram.command_wa = function (request) {
     if (config.url !== null
         && config.url.length > 0) {
-        //console.log("!wa: query:");
-        http.get(config.url + encodeURIComponent(request.args.join(" ")), function (res) {
-            //console.log("!wa: Got response: " + res.statusCode);
-            res.setEncoding('utf8');
 
-            res.on('data', function (chunk) {
-                plug.sendChat("/me " + chunk);
-            });
-        }).on('error', function (e) {
-            console.log("!wa: Got Error:" + e.message);
+        request(config.url + encodeURIComponent(request.args.join(" ")), function (error, response, body) {
+            if (error || response.statusCode != 200) {
+                console.error("!wa: Got Error:" + body);
+            }
+            else {
+                request.from.sendEmote("/me " + body);
+            }
         });
-    } else {
+    }
+    else {
         console.log("url set as: " + config.url);
     }
 };
 
-module.exports = {
-    "events": events
-};
+module.exports = wolfram;
