e82450822f6ab7c99315198e594b7541bad43c30
Joe Carter <theupperquartile@gmail.com>
2015-06-23 18:15:42 +0100

    Refactor rrd

--- plugins/rrdstats/rrd.js
+++ plugins/rrdstats/rrd.js
@@ -1,61 +1,53 @@
-var plugin;
-var config;
-var plug;
+var PluginInstance = require(__core + "PluginInstance.js");
+var rrd = new PluginInstance();
 
-var statsTimer;
+var request = require("request");
+var plugAPI = require("plugAPI");
 
-var events = {};
-
-events.onLoad = function (_plugin) {
-    plugin = _plugin;
-    manager = plugin.manager;
-    plug = manager.getPlugin("plug").plugin.plug;
-    config = plugin.getConfig();
-
+rrd.init = function() {
     statsTimer = setInterval(function () {
-        save_stats();
+        this.save_stats();
     }, 30 * 1000);
-    save_stats();
+    this.save_stats();
+
+    this.plug = this.manager.getPlugin("plug").plugin.plug; //TODO: implement better method
 };
 
-events.onUnload = function () {
+rrd.events.onUnload = function () {
     clearInterval(statsTimer);
 };
 
-events.plug_userJoin = function (user) {
-    save_stats();
+rrd.events.plug_userJoin = function (user) {
+    this.save_stats();
 };
 
-events.plug_userLeave = function (user) {
-    save_stats();
+rrd.events.plug_userLeave = function (user) {
+    this.save_stats();
 };
 
-events.plug_chat = function (message) {
+rrd.events.plug_chat = function (message) {
 
 };
 
-events.plug_advance = function (track) {
-    save_stats();
+rrd.events.plug_advance = function (track) {
+    this.save_stats();
 };
 
-function save_stats() {
+rrd.save_stats() {
     clearInterval(statsTimer);
     statsTimer = setInterval(function () {
         save_stats();
     }, 30 * 1000);
-
-    var http = require('http');
-    var url = require('url');
 
     if (config.url !== null
         && typeof config.url !== "undefined"
         && config.url.length > 0) {
-        var users = plug.getUsers();
+        var users = this.plug.getUsers();
         //console.log(users);
         var data = {};
-        data.djs = plug.getWaitList().length + (typeof plug.getDJ() === 'undefined' ? 0 : 1);
+        data.djs = this.plug.getWaitList().length + (typeof this.plug.getDJ() === 'undefined' ? 0 : 1);
         data.listeners = users.length;
-        data.guests = plug.getGuests();
+        data.guests = this.plug.getGuests();
         data.user = 0;
         data.residentDJ = 0;
         data.bouncer = 0;
@@ -69,65 +61,42 @@ function save_stats() {
         levels.sum = 0;
         for (var i = 0; i < users.length; ++i) {
             var rawrank = users[i].role;
-            if (users[i].gRole == "5") {
+            if (users[i].gRole == plugAPI.GLOBAL_ROLES.ADMIN) {
                 ++data.admin
             } else if (parseInt(users[i].gRole) > 1) {
                 ++data.brandAmbassador;
-            } else if (rawrank == 0) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.NONE) {
                 ++data.user;
-            } else if (rawrank == 1) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.RESIDENTDJ) {
                 ++data.residentDJ;
-            } else if (rawrank == 2) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.BOUNCER) {
                 ++data.bouncer;
-            } else if (rawrank == 3) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.MANAGER) {
                 ++data.manager;
-            } else if (rawrank == 4) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.COHOST) {
                 ++data.coHost;
-            } else if (rawrank == 5) {
+            } else if (rawrank == plugAPI.ROOM_ROLE.HOST) {
                 ++data.host;
             } else {
                 ++data.user;
                 console.log(users[i])
             }
             ++levels.count;
-            levels.sum = levels.sum + users[i].level;
-            data.avgLevel = levels.sum / levels.count;
-
+            levels.sum += users[i].level;
         }
-        var payload = "[" + JSON.stringify(data) + "]";
-        //console.log("rrdstats payload: "+payload);
+        data.avgLevel = levels.sum / levels.count;
 
-        //console.log("rrdstats query:" + config.url);
-        var link = url.parse(config.url, true, true);
-        var options = {
-            host: link.hostname,
-            port: link.port,
-            path: link.path,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': payload.length
+        var payload = JSON.stringify([data]);
+
+        request.post({
+            url: this.config.url,
+            body: data
+        }, function (err, httpResponse, body) {
+            if (error || response.statusCode != 200) {
+                console.error("rrd error:" + body);
             }
-        };
-        link = null;
-
-        var req = http.request(options, function (resp) {
-            //console.log('rrdstats response code: '+resp.statusCode);
-            resp.on('data', function (chunk) {
-                //console.log('BODY: ' + chunk);
-                //console.log('HEADERS: '+JSON.stringify(resp.headers));
-            })
-
-
         });
-        req.on('error', function (e) {
-            console.log('rrdstats error caught: ' + e.message);
-        });
-        req.write(payload);
-        req.end();
     }
 }
 
-module.exports = {
-    "events": events
-};
+module.exports = rrd;
