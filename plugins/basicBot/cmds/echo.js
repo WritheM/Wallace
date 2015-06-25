1a02ee3b343defe115f7d188a01ba5bd00c3f5c5
Joe Carter <theupperquartile@gmail.com>
2015-06-25 04:21:14 +0100

    restrict !say and !echo

--- plugins/basicBot/cmds/echo.js
+++ plugins/basicBot/cmds/echo.js
@@ -1,9 +1,15 @@
 module.exports = function (basicBot) {
 
     basicBot.events.command_echo = function (message) {
-        var text = message.message.substr(message.command.length + 2);
-        basicBot.plug.sendChat(text);
+        if (message.from.rank >= this.core.ranks.COHOST) {
+            var text = message.message.substr(message.command.length + 2);
+            basicBot.plug.sendChat(text);
 
-        basicBot.plug.moderateDeleteChat(message.raw.cid);
+            basicBot.plug.moderateDeleteChat(message.raw.cid);
+        }
+        else {
+            message.from.sendEmote("Command only available to staff");
+        }
     };
+
 };
