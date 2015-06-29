e6fca9357c9a5deeba2997b67ec787f16a5f6453
Joe Carter <theupperquartile@gmail.com>
2015-06-30 00:57:51 +0100

    port plug over to new plugin sys

--- coreplugins/plug/plug.js
+++ coreplugins/plug/plug.js
@@ -1,94 +1,83 @@
-var plugin;
-var events = {};
-
-var plug;
-var config;
-
+var PlugAPI = require("plugapi");
 var PlugUser = require("./PlugUser.js");
-var eventproxy = {};
 
-events.onLoad = function (_plugin) {
-    plugin = _plugin;
-    config = plugin.getConfig();
+var PluginInstance = require(__core + "PluginInstance.js");
+var plugin = new PluginInstance();
 
-    var PlugAPI = require("plugapi");
 
-    plug = new PlugAPI({
-        email: config.auth.email,
-        password: config.auth.password
+plugin.eventproxy = {};
+
+plugin.init = function () {
+    this.plug = new PlugAPI({
+        email: this.config.auth.email,
+        password: this.config.auth.password
     });
 
-    plug.multiLine = true;
-    plug.multiLineLimit = 5;
+    this.plug.multiLine = true;
+    this.plug.multiLineLimit = 5;
 
-    plug.connect(config.auth.room);
+    this.plug.connect(this.config.auth.room);
 
     //plug.on("roomJoin", eventproxy.roomJoin);
 
     for (var i in PlugAPI.events) {
         var event = PlugAPI.events[i];
-        if (!(event in eventproxy) && event !== "command") {
+        if (!(event in this.eventproxy) && event !== "command") {
             // javascript closure abuse: -
             // goal is to have "event" defined and to also pass that into the event
             // function
 
-            plug.on(event, function (event) {
+            this.plug.on(event, function (event) {
                 return function (arg) {
-                    eventproxy.generic(event, arg);
+                    plugin.eventproxy.generic(event, arg);
                 };
             }(event));
         }
         else {
-            plug.on(event, eventproxy[event]);
+            this.plug.on(event, this.eventproxy[event]);
         }
     }
 
 
     //plugAPI has a bug, raw command event double fires
     // bind specific command event also and filter out plain within func
-    plug.on("command:*", eventproxy["command"]);
-
-    module.exports.plug = plug;
+    this.plug.on("command:*", this.eventproxy["command"]);
 };
 
-eventproxy.generic = function (event, arg) {
+plugin.eventproxy.generic = function (event, arg) {
     //console.log("generic", event, arg);
     console.log("Event", event, arg);
     plugin.manager.fireEvent("plug_" + event, arg);
 };
 
-eventproxy.roomJoin = function (room) {
+plugin.eventproxy.roomJoin = function (room) {
     console.log("Joined " + room);
 
     plugin.manager.fireEvent("plug_roomJoin", room);
 };
 
-eventproxy.chat = function (message) {
+plugin.eventproxy.chat = function (message) {
     //var user = new PlugUser(message.from);
-    message.from = new PlugUser(message.from, plug);
+    message.from = new PlugUser(message.from, plugin.plug);
 
     plugin.manager.fireEvent("plug_chat", message);
     plugin.manager.fireEvent("chat", message);
 };
 
-eventproxy.command = function (message) {
+plugin.eventproxy.command = function (message) {
     if (this.event.indexOf(":") === -1) {
         return;
     }
 
-    message.from = new PlugUser(message.from, plug);
+    message.from = new PlugUser(message.from, plugin.plug);
 
     plugin.manager.fireEvent("plug_command_" + message.command, message);
     plugin.manager.fireEvent("command_" + message.command, message);
 };
 
 
-events.plug_chat = function (message) {
+plugin.events.plug_chat = function (message) {
     console.log("PlugChat: [@" + message.from.username + "] ", message.message);
 };
 
-module.exports = {
-    "events": events,
-    "plug": plug,
-    "PlugUser": PlugUser
-};
+module.exports = plugin;
