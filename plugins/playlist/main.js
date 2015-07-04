1c78e31586727379b6fcbeeb9947ec3fbfb10187
Joe Carter <theupperquartile@gmail.com>
2015-07-04 03:55:39 +0100

    Implement playlist plugin (closes #43)

---
+++ plugins/playlist/main.js
@@ -0,0 +1,87 @@
+var PluginInstance = require(__core + "PluginInstance.js");
+
+var playlist = new PluginInstance();
+
+var plugAPI = require("plugapi");
+
+playlist.init = function() {
+    this.plug = this.manager.getPlugin("plug").plugin.plug; //TODO: implement better method
+};
+
+var subcommands = {};
+
+playlist.events.command_playlist = function(message) {
+    if (message.from.rank >= playlist.core.ranks.MANAGER) {
+        var subcommand = message.args[0];
+        var func = subcommands.list;
+        if (subcommands[subcommand]) {
+            func = subcommands[subcommand];
+        }
+        func(message);
+    }
+    else {
+        message.from.sendEmote("Command only available to staff");
+    }
+};
+
+subcommands.list = function(message) {
+    playlist.plug.getPlaylists(function(playlists) {
+        var list = [];
+        for (var i in playlists) {
+            if (!playlists.hasOwnProperty(i)) {
+                continue;
+            }
+            var playlist = playlists[i];
+            var entry = playlist.id + "("+playlist.name+")";
+            if (playlist.active) {
+                entry = "*" + entry + "*";
+            }
+            list.push(entry);
+        }
+        message.from.sendReply(list.join(", "));
+    });
+};
+
+subcommands.activate = function(message) {
+    playlist.plug.activatePlaylist(message.args[1], function (err, data) {
+        if (err) {
+            message.from.sendReply("Error: "+err.message);
+        }
+        else {
+            message.from.sendReply("Playlist activated");
+        }
+    });
+};
+
+subcommands["delete"] = function(message) {
+    playlist.plug.deletePlaylist(message.args[1], function(err, data) {
+        if (err) {
+            message.from.sendReply("Error: "+err.message);
+        }
+        else {
+            message.from.sendReply("Playlist deleted");
+        }
+    });
+};
+
+subcommands.add = function(message) {
+    playlist.plug.createPlaylist(message.args[1], function(err, data) {
+      if (err) {
+          message.from.sendReply("Error: "+err.message);
+      }
+      else {
+          message.from.sendReply("Playlist created, ID: "+data[0].id);
+      }
+  });
+};
+
+playlist.events.command_grab = function(message) {
+    if (message.from.rank >= playlist.core.ranks.MANAGER) {
+        playlist.plug.grab();
+    }
+    else {
+        message.from.sendEmote("Command only available to staff");
+    }
+};
+
+module.exports = playlist;
