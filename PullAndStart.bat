57ebaef4fdb497733ee3c20b82a196635aa6eacc
Joe Carter <theupperquartile@gmail.com>
2015-07-31 17:19:55 +0100

    Try making pullandstart a little more speedy

--- PullAndStart.bat
+++ PullAndStart.bat
@@ -1,13 +1,19 @@
 @echo off
 :loop
-echo Updating from git's http remote...
-git fetch http
-git reset --hard http/master
-echo checking npm dependencies...
-cmd /c npm install
-echo Starting Wallace...
-title Wallace
-node main.js
-echo Pausing for 30 seconds to restart...
-PING 1.1.1.1 -n 1 -w 30000 >NUL
+    echo Updating from git's http remote...
+    set /p HEAD=<.git/refs/heads/master
+    git fetch http
+    git reset --hard http/master
+    set /p NEWHEAD=<.git/refs/heads/master
+    if %HEAD% == %NEWHEAD% goto start
+
+    echo checking npm dependencies...
+    cmd /c npm install
+
+    :start
+    echo Starting Wallace...
+    title Wallace
+    node main.js
+    echo Pausing for 30 seconds to restart...
+    PING 1.1.1.1 -n 1 -w 30000 >NUL
 goto loop
