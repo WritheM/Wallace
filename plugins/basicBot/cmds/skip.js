977c2c7ea83521ff6ad8ab1ee21daa47dc143e1f
Joe Carter <theupperquartile@gmail.com>
2015-08-07 01:27:37 +0100

    Fix skip command

--- plugins/basicBot/cmds/skip.js
+++ plugins/basicBot/cmds/skip.js
@@ -1,13 +1,13 @@
 module.exports = function (basicBot) {
     basicBot.events.command_skip = function (message) {
-        var dj = this.plug.getDJ();
+        var dj = this.plug.getCurrentDJ();
         if (typeof dj !== "undefined" && (message.from.rank >= this.core.ranks.BOUNCER || dj.id === message.from.id)) {
-            this.plug.sendChat("/me [" + message.from.username+" used skip, it was very effective!]");
-            this.plug.moderateForceSkip();
+            this.plug.room.sendChat("/me [" + message.from.username+" used skip, it was very effective!]");
+            this.plug.skipDJ(dj);
 
             //TODO: implement timeout so it does things in the proper order.
             if (message.args.length !== 0) {
-                this.plug.sendChat("/me @" + this.plug.getDJ().username+" "+ this.config.skip.reasons[message.args[0]]);
+                this.plug.room.sendChat("/me @" + this.plug.getDJ().username+" "+ this.config.skip.reasons[message.args[0]]);
             }
 
         }
