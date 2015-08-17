18133c8f0a07051c310112b5d95a8bb56086796b
Joe Carter <theupperquartile@gmail.com>
2015-08-17 06:27:52 +0100

    Add CommandHandler decoration

--- core/Plugin/CommandHandler.js
+++ core/Plugin/CommandHandler.js
@@ -1,3 +1,51 @@
-/**
- * Created by joe on 17/08/15.
- */
+export default function CommandHandler(name, opts) {
+    opts = opts || {};
+
+    return function (target, method, descriptor) {
+        if (!name) {
+            name = method;
+        }
+        if (!target.startevents) {
+            target.startevents = [];
+        }
+
+        let event;
+        if (opts.source) {
+            event = opts.source + "_command_" + name;
+        }
+        else {
+            event = "command_" + name;
+        }
+
+        let func;
+        if (opts.rank) {
+            func = function (messageData) { // `this` is equal to plugin class
+                let required = this.core.ranks.matchByName(opts.rank);
+                if (messageData.from.rank < required.value) {
+                    let error = "You do not have permission ({access})";
+                    if (opts.rankmsg) {
+                        error = opts.rankmsg;
+                    }
+
+                    error = error.split("{access}").join(messageData.from.rank+"/"+required.value);
+
+                    messageData.from.sendReply(error);
+                }
+                else {
+                    //user has permission, call original func
+                    descriptor.value.apply(this, arguments);
+                }
+            };
+        }
+        else {
+            func = descriptor.value;
+        }
+
+        console.log(name, opts, event);
+
+        //same as EventHandler for now
+        let events = target.startevents.slice();
+        events.push({"event": event, "func": func});
+        target.startevents = events;
+    };
+}
